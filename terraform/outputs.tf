0434741733de1e6d46dd093aa1d2c650fe6cc173
Iain <icd@wan0.net>
2026-03-29 17:01:51 +1100

    Rename yeet → sharkcage

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vms" {
-  value = {
-    for name, vm in proxmox_virtual_environment_vm.yeet : name => {
-      id   = vm.vm_id
-      ip   = local.vms[name].ip
-      node = local.vms[name].node
-    }
-  }
-}
